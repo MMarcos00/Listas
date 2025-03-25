d32f2ba817a7ce6f42cad5d4a483820861ee0380
MMarcos00 <mendezmarcosgt@gmail.com>
2025-03-24 23:40:54 -0600

    Lista doblemente enlazada

--- src/main/java/umg/edu/progra/listas/Principal.java
+++ src/main/java/umg/edu/progra/listas/Principal.java
@@ -1,5 +1,8 @@
 package umg.edu.progra.listas;
-//Fabian OSITO --- MARCOS MK
+
+import umg.edu.progra.listas.doblementeEnlazada.DoblementeEnlazada;
+
+// Fabian OSITO --- MARCOS MK
 public class Principal {
     public static void main(String[] args) {
         Lista lista = new Lista();
@@ -36,5 +39,35 @@ public class Principal {
 
         System.out.println("\nLista de impares:");
         listaImpares.visualizar();
+
+        // Implementación de la lista doblemente enlazada
+        DoblementeEnlazada listaDoble = new DoblementeEnlazada();
+        listaDoble.insertAtBeginning(1);
+        listaDoble.insertAtBeginning(2);
+        listaDoble.insertAtBeginning(3);
+        listaDoble.insertAtBeginning(4);
+        listaDoble.insertAtBeginning(5);
+        listaDoble.insertAtBeginning(6);
+
+        System.out.println("\nLista doblemente enlazada original:");
+        listaDoble.displayForward();
+
+        // Método 1: Contar el número de nodos en la lista
+        System.out.println("\nNúmero de nodos en la lista doblemente enlazada: " + listaDoble.countNodes());
+
+        // Método 2: Insertar un nodo después de un valor específico
+        int valorReferencia = 3;
+        int nuevoValor = 99;
+        if (listaDoble.insertAfter(valorReferencia, nuevoValor)) {
+            System.out.println("\nLista doblemente enlazada después de insertar " + nuevoValor + " después de " + valorReferencia + ":");
+            listaDoble.displayForward();
+        } else {
+            System.out.println("\nNo se encontró el valor " + valorReferencia + " en la lista doblemente enlazada.");
+        }
+
+        // Método 3: Revertir la lista doblemente enlazada
+        listaDoble.reverse();
+        System.out.println("\nLista doblemente enlazada revertida:");
+        listaDoble.displayForward();
     }
 }
